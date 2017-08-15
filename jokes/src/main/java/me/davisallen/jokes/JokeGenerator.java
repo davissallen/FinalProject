596d8c3ecda6a3a965b1ab0eba33973ea0df1093
Davis Allen <davissallen@gmail.com>
2017-08-15 13:36:55 -0700

    Initial commit

---
+++ jokes/src/main/java/me/davisallen/jokes/JokeGenerator.java
@@ -0,0 +1,13 @@
+package me.davisallen.jokes;
+
+public class JokeGenerator {
+
+    public JokeGenerator() { }
+
+    public String generateJoke() {
+        return "Why did the dolphin raise his fin?\n\nTo live it up!!!";
+    }
+
+    // TODO (1): Make better jokes... seriously.
+    // TODO (2): Create a list of jokes or random joke generator to send.
+}
